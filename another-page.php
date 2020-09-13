bb4dbbbefaceeb8dccdc11c16776d49e6ff4f6a4
GaganAgarwal1813 <48082205+GaganAgarwal1813@users.noreply.github.com>
2020-09-13 11:46:52 +0530

    Add files via upload

---
+++ another-page.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+<div style="margin-top: 5vh;">
+    <img src="logo.png" style="display: block;margin-left: auto;margin-right: auto;width: 50%;" >
+</div>
+<div style="margin-bottom:35vh;">
+    <h2 style="text-align: center;font-family: sans-serif;margin-top:10vh;">Welcome To AdmitKard</h2>
+    <h4 style="color: gray; font-family: sans-serif; text-align: center;">In order to provide you with a custom experience, 
+        <h4 style="font-family: sans-serif; text-align: center;">we need to ask you a few questions
+        </h4>
+    </h4>
+</div>
+
+<button type="submit" name="submit" class="btn btn-primary" style="border-radius: 20px; background-color: orange;display: block;margin-left: auto;margin-right: auto;width: 15%;border:0px;">
+<p style="color: white; margin: 0px;padding:5px;">
+    Get Started
+</p>
+</button>
+<p style="color: gray; font-family: sans-serif; text-align: center;font-size:15px;">
+    This will only take 5 minutes
+</p>
+</body>
+</html>
+<?php 
+
+?>
---
+++ credential.php
@@ -0,0 +1,7 @@
+<?php 
+	/*API_KEY*/
+	define("API_KEY", '8Le8+CmPH9E-zc03FooZqeeM0PhKhmPkgjghZXCGQS');
+
+	/*You can enter mobile number here*/
+	define("MOBILE", '');
+ ?>
